6a7faed1e3ffe1e9219db79640726f15aa1b83df
Spiderdude1 <zakiahmed1422@live.com>
2022-10-02 14:37:54 -0700

    Final V 1.0.0

--- Carrington-EDA.py
+++ Carrington-EDA.py
@@ -1,4 +1,6 @@
-from re import X
+from cProfile import label
+from email import header
+from time import tzname
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -6,59 +8,46 @@ import os
 os.environ['CDF_LIB'] = '~/CDF/lib'
 from spacepy import pycdf
 import cdflib
+from dtw import *
+import datetime
+dscovr_cdf = pycdf.CDF('dscovr_h0_mag_20220101_v01.cdf')
+# print(dscovr_cdf['Epoch1'][...])
+wind_cdf = pycdf.CDF('wi_h2_mfi_20220101_v04.cdf')
+dscovr_cpy  = dscovr_cdf.copy()
+wind_cpy = wind_cdf.copy()
 
-plt.style.use('_mpl-gallery')
+discover2_df = pd.DataFrame(dscovr_cdf['B1GSE'], columns= ['x', 'y', 'z'])
+# pick only every 100 rows
+discover2_df = discover2_df[0::50]
+discover2_df['amp'] = np.sqrt(discover2_df['x'] ** 2 + discover2_df['y'] ** 2 + discover2_df['z'] ** 2)
+discover2_df['time'] = datetime.datetime(2022, 1, 1, 0, 0, 0).timestamp() + (discover2_df.index-1)
+# pd.to_datetime(dscovr_cdf['Epoch1'].tolist()._type('int64')) // 10**9
+pd.options.display.float_format = '{:.5f}'.format
+discover2_df['date'] = pd.to_datetime(discover2_df['time'],unit='s', utc=True)
+discover2_df = discover2_df.drop(discover2_df[discover2_df.amp >=999].index)
 
-cdf_pycdf = pycdf.CDF('dscovr_h0_mag_20220101_v01.cdf')
+print(discover2_df.head(5))
+print(discover2_df.tail(5))
 
-cdf_pycdf2 = pycdf.CDF('wi_h2_mfi_20220101_v04.cdf')
-cdf_data  = cdf_pycdf.copy()
-cdf_data2 = cdf_pycdf2.copy()
-# print('These are the variables within this file:\n')
-arr = cdf_pycdf2['BGSE'][...]
-arr[arr > 999] = np.NaN
-arr[arr < -999] = np.NaN
-# print(cdf_pycdf2['BGSE'][...])
+# wind_df = pd.DataFrame(wind_cdf['BGSE'][:100], columns= ['x', 'y', 'z'])
+wind_df = pd.DataFrame(wind_cdf['BGSE'], columns= ['x', 'y', 'z'])
+wind_df = wind_df[0::50]
+wind_df['amp'] = np.sqrt(wind_df['x'] ** 2 + wind_df['y'] ** 2 + wind_df['z'] ** 2)
+wind_df['time'] = datetime.datetime(2022, 1, 1, 0, 0, 0).timestamp() + (wind_df.index-1) / 10.715
+wind_df['time'] = wind_df['time']
+wind_df['date'] = pd.to_datetime(wind_df['time'],unit='s', utc=True)
+wind_df = wind_df.drop(wind_df[wind_df.amp >=999].index)
+print(wind_df.head(5))
+print(wind_df.tail(5))
+print(wind_df.count)
 
-
-# print(cdf_pycdf.attrs)
-
-# print(cdf_pycdf['SENS'])
-# print(cdf_pycdf2)
-# print(cdf_data.keys())
-
-# wind_cdf_data = cdflib.cdf_to_xarray('wi_h2_mfi_20220101_v04.cdf', fillval_to_nan=True)
-
-# df = pd.DataFrame(wind_cdf_data)
-# print(df)
-
-# print(cdf_data['B1GSE'][...])
-# print(cdf_data['B1GSE'].attrs)
-# arr = np.array(cdf_data['B1GSE'][:, 2])
-
-x = cdf_data['Epoch1']
-
-# y = cdf_data['B1GSE'][:, 2]
-#message
-fig, ax = plt.subplots()
-
-ax.plot(x, arr, linewidth=2.0)
-
-
-
-
-
-
-# print(arr)
-
-# print(cdf_data['B1GSE'].attrs)
-
-# print(cdf_data2.keys())
-
-# plt.plot(cdf_data['Epoch1'][...])
-
-
-cdf_pycdf.close()
-cdf_pycdf2.close()
-
+plt.style.use('ggplot')
+plt.plot(discover2_df.date, discover2_df.amp, label="DSCOVR Satellite (L1)")
+plt.plot(wind_df.date, wind_df.amp, label='Wind Satellite (L2)')
+plt.xlabel('Time (Datetime format: PST)')
+plt.ylabel('Amplitude of magnetic flux (in nanotesla)')
+plt.legend()
 plt.show()
+
+dscovr_cdf.close()
+wind_cdf.close()
